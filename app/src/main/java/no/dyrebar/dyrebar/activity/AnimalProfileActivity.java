39721b6ae632ff713262323037a60789188ec30e
Melfyn <melfyn1990@hotmail.com>
2020-01-31 14:43:04 +0100

    view animal profile layout completed

---
+++ app/src/main/java/no/dyrebar/dyrebar/activity/AnimalProfileActivity.java
@@ -0,0 +1,17 @@
+package no.dyrebar.dyrebar.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import no.dyrebar.dyrebar.R;
+
+public class AnimalProfileActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animal_profile);
+    }
+}
