c815e67b0635841be8062cd5ecf4d7045e138ea4
Olav <olavrui@gmail.com>
2020-02-04 11:19:35 +0100

    Updates

--- app/src/main/java/no/dyrebar/dyrebar/classes/Found.java
+++ app/src/main/java/no/dyrebar/dyrebar/classes/Found.java
@@ -2,5 +2,14 @@ package no.dyrebar.dyrebar.classes;
 
 public class Found extends Poster
 {
-    
+    private int userId;
+    private String animalType;
+    private String sex;
+    private String sterilized;
+    private String color;
+    private String furLength;
+    private String furPattern;
+    private String description;
+
+
 }
--- app/src/main/java/no/dyrebar/dyrebar/fragment/HomeFragment.java
+++ app/src/main/java/no/dyrebar/dyrebar/fragment/HomeFragment.java
@@ -12,14 +12,6 @@ import no.dyrebar.dyrebar.R;
 public class HomeFragment extends Fragment
 {
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
