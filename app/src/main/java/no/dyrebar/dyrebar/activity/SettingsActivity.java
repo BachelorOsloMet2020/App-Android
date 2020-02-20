252e66da54e9f03ad7344254d924a6c3462256c5
Melfyn <melfyn1990@hotmail.com>
2020-02-20 15:15:58 +0100

    Settins layout

---
+++ app/src/main/java/no/dyrebar/dyrebar/activity/SettingsActivity.java
@@ -0,0 +1,17 @@
+package no.dyrebar.dyrebar.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import no.dyrebar.dyrebar.R;
+
+public class SettingsActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
