e7b4ec566357ea84c086e7684f9ee06456d12114
olavrui <olavrui@gmail.com>
2020-02-25 10:08:23 +0100

    Missing adapter and MissingDialog updated

---
+++ app/src/main/java/no/dyrebar/dyrebar/adapter/SimpleMissingPosterAdapter.java
@@ -0,0 +1,16 @@
+package no.dyrebar.dyrebar.adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import no.dyrebar.dyrebar.R;
+
+public class SimpleMissingPosterAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adapter_simple_missing_poster);
+    }
+}
--- app/src/main/java/no/dyrebar/dyrebar/dialog/MissingDialog.java
+++ app/src/main/java/no/dyrebar/dyrebar/dialog/MissingDialog.java
@@ -2,29 +2,49 @@ package no.dyrebar.dyrebar.dialog;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import no.dyrebar.dyrebar.R;
+import no.dyrebar.dyrebar.classes.Missing;
 
 public class MissingDialog
 {
-    private Activity activity;
     private Dialog dialog;
     private View v;
+    private ListView listView;
+    private Context context;
 
-    public MissingDialog(Activity activity, String title)
+    public MissingDialog(Context context, String title)
     {
-        this.activity = activity;
+        this.context = context;
         dialog = make();
         ((TextView)v.findViewById(R.id.dialog_missing_title)).setText(title);
+        listView.findViewById(R.id.dialog_listview_animals);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Missing m = parent.getAdapter().getItem(position);
+            }
+        });
+
+        //v.findViewById(R.id.dialog_missing_btn_confirm).setOnClickListener();
+        //v.findViewById(R.id.dialog_missing_btn_cancel).setOnClickListener();
 
     }
 
     private Dialog make()
     {
-        v = View.inflate(activity, R.layout.dialog_missing, null);
-
+        v = View.inflate(context, R.layout.dialog_missing, null);
+        Dialog dialog = new Dialog(context, R.style.DialogIndicator);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(v);
         return dialog;
     }
 
